f822da9c9e161c754d416803327d691d00fb58c1
Saihaj Law <saihajlaw@Saihajs-MacBook-Air.local>
2024-02-08 22:11:39 -0500

    FIXES FOR DEPLOYMENT

--- src/components/dom/Loader.tsx
+++ src/components/dom/Loader.tsx
@@ -38,7 +38,7 @@ export const Loader: React.FC<LoaderProps> = ({ setUnmount }) => {
         style={textSpring1}
         className='relative text-3xl text-white font-fog bottom-8'
       >
-        Saihaj <i>'SLANO'</i> Law
+        Saihaj <i>&lsquoSLANO&lsquo</i>  Law
       </a.span>
     </a.div>
   )
